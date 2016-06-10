d975c142db78ff61e65eca7f0940558554afe429
Drew Baugher <drewbaugher@Drews-MacBook-Pro.local>
2016-06-09 19:37:40 -0700

    [Add][Layout] Adds login screen and login styling

--- src/components/app.js
+++ src/components/app.js
@@ -3,7 +3,28 @@ import React, { Component } from 'react';
 export default class App extends Component {
   render() {
     return (
-      <div>Techprep Initial Setup</div>
+      <div className="wrapper">
+        <div className="login">
+          <h1>Welcome</h1>
+          <form className="form">
+            <input type="text" placeholder="Username" />
+            <input type="password" placeholder="Password" />
+            <button type="submit" id="login-button">Login</button>
+          </form>
+        </div>
+        <ul className="bg-bubbles">
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
     );
   }
 }
