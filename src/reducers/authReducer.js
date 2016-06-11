835e390ab841995e27a0291740e1189165d1a776
Drew Baugher <drewbaugher@Drews-MacBook-Pro.local>
2016-06-10 19:42:03 -0700

    [Add][Layout] Adds new files for file structure. Updates styling for header, landingpage, login, signup, welcome components. Adds naive authentication flag with reducer/state. Adds action creators.

---
+++ src/reducers/authReducer.js
@@ -0,0 +1,12 @@
+export default function(state = {}, action) {
+  switch(action.type){
+    case 'AUTHORIZE_USER':
+      return {...state, error: '', authenticated: true };
+    case 'DEAUTHORIZE_USER':
+      return {...state, authenticated: false };
+    case 'AUTHORIZE_ERROR':
+      return {...state, error: action.payload};
+    default:
+      return state;
+  }
+}
