0f61205cb9902e537c802ed2c98863aa53c369da
jcursoli <joeycursoli2@gmail.com>
2016-06-14 21:49:04 -0700

    [Pull] Builds out practice component

--- src/components/practice.js
+++ src/components/practice.js
@@ -7,11 +7,9 @@ import Subheader from 'material-ui/Subheader';
 import StarBorder from 'material-ui/svg-icons/toggle/star-border';
 
 
-	const contents = [
-										{title: '<html/>', pic:'https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/2000px-HTML5_logo_and_wordmark.svg.png'},
-										{title: 'CSS', pic:'http://wpguru.co.uk/wp-content/uploads/2013/09/CSS-Logo-214x300.png'},
-										{title: 'Javascript', pic: 'http://ric.mclaughlin.today/assets/themes/ricify/images/javascript.png'}
-										];
+	const contents = [ {title: '<html/>', pic:'http://ademocut.com/wp-content/uploads/2015/02/HTML.jpg'},
+										{title: 'CSS', pic:'http://thecybertramp.com/blog/wp-content/uploads/2011/08/rainbow.jpg'},
+										{title: 'Javascript', pic: 'http://linpossible.com/img/skills/javascript_logo.png'} ];
 
 const style = {
   root: {
@@ -29,14 +27,20 @@ const style = {
     marginBottom: 24,
     marginTop: 35,
   },
-
 };
+
 export default class Practice extends Component {
+	
+	handleClick(target){
+		console.log('handleClick clicked now',target);
+	};
+
 	renderElements(category){
 		console.log(category);
 		return (
 			     <GridTile
-			     	style={style.GridTile}
+			     	 onTouchTap={this.handleClick.bind(this)}
+			     	 style={style.GridTile}
 			       key={category.title}
 			       title={category.title}
 			       subtitle={<span>by <b>{category.title}</b></span>}
@@ -44,10 +48,8 @@ export default class Practice extends Component {
 			     	>
 			       <img height='100%'width='100%' src={category.pic} />
 			     </GridTile>
-
 		);
-	}
-
+	};
 	render(){
 		return(
 			<div>
