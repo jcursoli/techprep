e8671f4b6fbdb048178b75a6bd3732c1c6d5a0f8
Doug Cole <idugcoal@gmail.com>
2016-06-17 00:01:57 -0700

    [Update] Shows basic comments in answer dialog

--- src/components/comments.js
+++ src/components/comments.js
@@ -1,25 +1,58 @@
 import React, { Component } from 'react';
 import {Card, CardActions, CardHeader, CardText} from 'material-ui/Card';
 import FlatButton from 'material-ui/FlatButton';
-
+import {List, ListItem} from 'material-ui/List';
+import Divider from 'material-ui/Divider';
 export default class Comments extends Component {
-  
+  constructor(props) {
+    super(props);
+    this.state = {
+      expanded: false,
+    };
+  }
 
 
   render() {
+
+    const commentList = [
+      { username: 'jesus', comment: 'whoa guys cool app' },
+      { username: 'bob', comment: 'wow you guys are super cool'},
+      { username: 'jesus', comment: 'whoa guys cool app' },
+      { username: 'bob', comment: 'wow you guys are super cool'},
+      { username: 'jesus', comment: 'whoa guys cool app' },
+      { username: 'bob', comment: 'wow you guys are super cool'},
+      { username: 'jesus', comment: 'whoa guys cool app' },
+      { username: 'bob', comment: 'wow you guys are super cool'}
+    ];
+
     return (  
       <Card>
         <CardHeader
-          title="Without Avatar"
-          subtitle="Subtitle"
+          title="Comments"
+          subtitle=""
           actAsExpander={true}
           showExpandableButton={true}
         />
         <CardText expandable={true}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
-          Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
-          Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+          {commentList.map((row, index) => {
+            
+            // const handleOpen = () => {
+            //   this.setState({
+            //     username: commentList['username'], 
+            //     comment: commentList['comment']
+            //   });
+
+            return (
+              <List key={index}>
+                <ListItem>{row.username}</ListItem>
+                <ListItem>{row.comment}</ListItem>
+                <Divider />
+              </List>
+            );
+          })}
+
+
+
         </CardText>
         <CardActions expandable={true}>
           <FlatButton label="Action1" />
--- src/components/questions.js
+++ src/components/questions.js
@@ -78,12 +78,10 @@ export default class Questions extends Component  {
         keyboardFocused={true}
         onTouchTap={this.showAnswer}
       />,
+      <Comments />
     ];
     
-    const comments = [
-      { username: 'brad', comment: 'hi guys' },
-      { username: 'peter', comment: 'hello'}
-    ];
+    
 
     return (
       <div>
@@ -97,7 +95,7 @@ export default class Questions extends Component  {
             onRequestClose={this.handleClose}
           >
             <div id="answer"> {this.state.revealAnswer ? this.state.answer : ''} </div>
-            <Comments hintText="Show Comments" />
+            
           </Dialog>
             </div>
         </div>
