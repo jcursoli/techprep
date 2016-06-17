a0b919c3ac5af9254fcb05f4165c96b248b63809
jcursoli <joeycursoli2@gmail.com>
2016-06-17 16:06:26 -0700

    [Pull] Creates an output area for the users code to show

--- src/components/algorithms.js
+++ src/components/algorithms.js
@@ -10,24 +10,37 @@ import 'brace/mode/ruby';
 
 
 const style = {
+	output:{
+		width:'200px',
+		height: '308px',
+		marginTop:'25px',
+		marginBottom:'20px',
+		border:' 4px solid #EEEFF2',
+		background:'black',
+		color:'red', 
+		display:'inline',
+		fontweight: '600',
+	},
+	compiler:{
+		display: 'flex',
+		alignItems: 'center',
+	},
 	button:{
 		display: 'flex',
 		flexDirection: 'column',
 		alignItems: 'center',
-		width: '100px', 
+		width: '10%px', 
 		height: '30px',
 		margin: '20px', 
 		padding: '4px',
 		margin:' 20px',
 	},
 	editor:{
-		height: '300px',
-    display: 'flex',
-		flexDirection: 'column',
+		height: '308px',
 		marginTop:'25px',
-		alignItems: 'center',
 		marginBottom:'20px',
 		border:' 4px solid #EEEFF2',
+		alignItems: 'center',
 	}
 }
 
@@ -38,7 +51,8 @@ class Algorithms extends Component {
 	super(props);
 	this.state = {
 		editorContents: 'function toyProblem(a,b){ \n //enter code here \n }',
-		language: 'javascript'
+		language: 'javascript',
+		error: "Lorem ipsum dolor sit amet, consectetur adipiscing elit,"
 		};
 		this.editorChanged = this.editorChanged.bind(this);
 		this.runCode = this.runCode.bind(this);
@@ -47,7 +61,20 @@ class Algorithms extends Component {
 		this.setState({ editorContents })
 	}
 	runCode(){
-		console.log('this is the editor contents',this.state.editorContents)
+		//new Function ([arg1[, arg2[, ...argN]],] functionBody)
+		var userFunction;
+		var index = this.state.editorContents.indexOf('{');
+		var lastIndex = this.state.editorContents.lastIndexOf('}');
+		var functionBody = this.state.editorContents.substring(index+1,lastIndex);
+		try{
+			this.setState({error:''});
+				userFunction = new Function(functionBody);
+		}
+		catch(err){
+			console.log(err);
+			this.setState({error:'Syntax Error cannot run'});
+		}
+		console.log('this is the editor contents',userFunction);
 	}
 
 	render(){
@@ -58,17 +85,21 @@ class Algorithms extends Component {
 					<div  style={{color:'black', margin:'20px'}}>
 						"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 					</div>
-						<div style={style.editor}>
-							<AceEditor
-								fontSize={10}
-								value={this.state.editorContents}
-							  mode="javascript"
-							  theme="github"
-							  onChange={this.editorChanged}
-							  name="ACE_EDITOR"
-							  editorProps={{$blockScrolling: true}}
-							/>
-						</div>
+						<span style={style.compiler}>
+							<div style={style.output}>{this.state.error}</div>
+							<div style={style.editor}>
+								<AceEditor
+									height={'300px'}
+									fontSize={10}
+									value={this.state.editorContents}
+								  mode="javascript"
+								  theme="github"
+								  onChange={this.editorChanged}
+								  name="ACE_EDITOR"
+								  editorProps={{$blockScrolling: true}}
+								/>
+							</div>
+						</span>
 						<button onClick={this.runCode} style={style.button}>Run</button>
 				</div>
 			</div>
