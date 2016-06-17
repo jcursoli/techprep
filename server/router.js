9cae259f44239834ae5609ba0a29ff8511e9d81e
Drew Baugher <drewbaugher@Drews-MacBook-Pro.local>
2016-06-16 19:37:05 -0700

    [Delete][Cleanup] Deletes and removes firebase and auth from server files. Cleans up router file.

--- server/router.js
+++ server/router.js
@@ -1,9 +1,4 @@
-const Authentication = require('./controllers/authentication');
-const passportService = require('./services/passport');
-const passport = require('passport');
 const path = require('path');
-const requireAuth = passport.authenticate('jwt', { session: false });
-const requireSignin = passport.authenticate('local', { session: false });
 
 module.exports = function(app) {
   app.get('/', function(req, res) {
@@ -18,8 +13,6 @@ module.exports = function(app) {
   app.get('/style/flexboxgrid.css', function(req, res) {
     res.sendFile(path.resolve(__dirname + '/../style/flexboxgrid.css'));
   });
-  app.post('/signin', requireSignin, Authentication.signin);
-  app.post('/signup', Authentication.signup);
 
   app.get('/*', function(req, res) {
     res.sendFile(path.resolve(__dirname + '/../index.html'));
