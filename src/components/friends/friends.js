fd05a1b28f99963cdf52c75a6e45917b1f4cdd43
Drew Baugher <dmusicb@users.noreply.github.com>
2016-06-17 14:09:34 -0700

    [Add] Adds friends drawer

--- src/components/friends/friends.js
+++ src/components/friends/friends.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import * as actions from '../../actions';
 import {List, ListItem} from 'material-ui/List';
 
+import Subheader from 'material-ui/Subheader';
 import Avatar from 'material-ui/Avatar';
 import {grey400, darkBlack, lightBlack} from 'material-ui/styles/colors';
 import IconButton from 'material-ui/IconButton';
@@ -15,7 +16,7 @@ import Divider from 'material-ui/Divider';
 const iconButtonElement = (
   <IconButton
     touch={true}
-    tooltip="more"
+    tooltip="Options"
     tooltipPosition="bottom-left"
   >
     <MoreVertIcon color={grey400} />
@@ -24,87 +25,53 @@ const iconButtonElement = (
 
 const rightIconMenu = (
   <IconMenu iconButtonElement={iconButtonElement}>
-    <MenuItem>Reply</MenuItem>
-    <MenuItem>Forward</MenuItem>
-    <MenuItem>Delete</MenuItem>
+    <MenuItem>Message</MenuItem>
+    <MenuItem>View Profile</MenuItem>
+    <MenuItem>Remove Friend</MenuItem>
   </IconMenu>
 );
 
-class Chat extends Component {
+class Friends extends Component {
   constructor(props) {
     super(props);
   }
 
-  renderChat() {
-    //render current chat chronologically
+  renderFriends() {
+    //render current friends alphabetically
   }
 
   render() {
+    // add search for new friends to add
     return (
       <List>
+        <ListItem
+          leftAvatar={<Avatar src="https://avatars2.githubusercontent.com/u/7004741?v=3&s=460" />}
+          rightIconButton={rightIconMenu}
+          primaryText="Drew Baugher"
+        />
+        <Divider inset={false} />
         <ListItem
           leftAvatar={<Avatar src="https://avatars3.githubusercontent.com/u/10988122?v=3&s=460" />}
           rightIconButton={rightIconMenu}
           primaryText="Joey Cursoli"
-          secondaryText={
-            <p>
-            <span style={{color: 'black'}}>help me please!?</span><br />
-              I don't know what I'm doing
-            </p>
-          }
-          secondaryTextLines={2}
         />
         <Divider inset={false} />
         <ListItem
           leftAvatar={<Avatar src="https://avatars3.githubusercontent.com/u/9814083?v=3&s=460" />}
           rightIconButton={rightIconMenu}
           primaryText="Doug Cole"
-          secondaryText={
-            <p>
-            <span style={{color: 'black'}}>yo dude</span><br />
-              i solved the middle out algorithm
-            </p>
-          }
-          secondaryTextLines={2}
         />
         <Divider inset={false} />
         <ListItem
           leftAvatar={<Avatar src="https://avatars1.githubusercontent.com/u/4237388?v=3&s=460" />}
           rightIconButton={rightIconMenu}
           primaryText="Forrest Akin"
-          secondaryText={
-            <p>
-            <span style={{color: 'black'}}>heyooo</span><br />
-              I can't find my shirt, do you know where I left it?
-            </p>
-          }
-          secondaryTextLines={2}
         />
         <Divider inset={false} />
         <ListItem
           leftAvatar={<Avatar src="https://avatars2.githubusercontent.com/u/16884524?v=3&s=460" />}
           rightIconButton={rightIconMenu}
           primaryText="Colin Zarnegar"
-          secondaryText={
-            <p>
-            <span style={{color: 'black'}}>React help</span><br />
-              Hey, can you help me with React
-            </p>
-          }
-          secondaryTextLines={2}
-        />
-        <Divider inset={false} />
-        <ListItem
-          leftAvatar={<Avatar src="https://avatars2.githubusercontent.com/u/7004741?v=3&s=460" />}
-          rightIconButton={rightIconMenu}
-          primaryText="Drew Baugher"
-          secondaryText={
-            <p>
-            <span style={{color: 'black'}}>Re: Question</span><br />
-              I created Google.
-            </p>
-          }
-          secondaryTextLines={2}
         />
         <Divider inset={false} />
       </List>
@@ -113,7 +80,7 @@ class Chat extends Component {
 }
 
 function mapStateToProps(state) {
-  return { chat: state.chat };
+  return { friends: state.friends };
 }
 
-export default connect(mapStateToProps, actions)(Chat);
+export default connect(mapStateToProps, actions)(Friends);
