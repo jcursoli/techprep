d206f40d8a05d87cd022b9e0797afbb1623bec52
jcursoli <joeycursoli2@gmail.com>
2016-06-20 22:09:20 -0700

    [Add] Adds profile features to display users details

--- src/components/app.js
+++ src/components/app.js
@@ -5,7 +5,7 @@ import Header from './header';
 export default class App extends Component {
   render() {
     return (
-      <div style={{height:'100%'}}>
+      <div>
         <Header />
         {this.props.children}
       </div>
