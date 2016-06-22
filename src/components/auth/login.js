7e52e7aa603700f663d0ca4b7fdef8a72ff0bda7
Drew Baugher <dmusicb@users.noreply.github.com>
2016-06-22 14:02:16 -0700

    [Add] Adds form validation for login

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import { reduxForm } from 'redux-form';
 import * as actions from '../../actions';
 
+
 class Login extends Component {
   handleFormSubmit({ email, password }) {
     this.props.loginUser({ email, password });
@@ -21,8 +22,8 @@ class Login extends Component {
       <div className="login">
         <h1>Welcome back</h1>
         <form className="form" onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
-          <input {...email} type="text" placeholder="Email" />
-          <input {...password} type="password" placeholder="Password" />
+          <input className={email.touched && email.error && 'errorColor'} {...email} type="text" placeholder={(email.touched && email.error) ? email.error : 'Email'} />
+          <input className={password.touched && password.error && 'errorColor' } {...password} type="password" placeholder={(password.touched && password.error) ? password.error : 'Password'} />
           { this.renderError() }
           <button type="submit" id="login-button">Login</button>
         </form>
@@ -35,11 +36,16 @@ function mapStateToProps(state){
   return {};// show error message
 }
 
-function validate(formProps) {
+function validate(values) {
   const errors = {};
-
-  // validation here
-
+  if (!values.email) {
+    errors.email = 'Required';
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = 'Invalid email address';
+  }
+  if (!values.password) {
+    errors.password = 'Required';
+  }
   return errors;
 }
 
