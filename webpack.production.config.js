5007ee1ae196f3f97516fb0df092089b68d7080f
Drew Baugher <dmusicb@users.noreply.github.com>
2016-06-24 08:59:34 -0700

    [Add] Adds webpack production config

---
+++ webpack.production.config.js
@@ -0,0 +1,44 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+  entry: [
+    './src/index'
+  ],
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: [
+          'babel-loader',
+        ],
+        exclude: /node_modules/,
+      },
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  devtool: 'source-map',
+  plugins: [
+      new webpack.NoErrorsPlugin(),
+      new webpack.DefinePlugin({
+        'process.env.NODE_ENV': '"production"'
+      }),
+      new webpack.optimize.OccurenceOrderPlugin(true),
+      new webpack.optimize.AggressiveMergingPlugin(),
+      new webpack.optimize.DedupePlugin(),
+      new webpack.optimize.UglifyJsPlugin({ //minify all the things
+        compress: { warnings: false },
+        comments: false,
+        sourceMap: true,
+        mangle: true,
+        minimize: true
+      }),
+    ],
+};
